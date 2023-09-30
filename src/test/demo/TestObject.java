65ba6b43dc30e5ebd457bc41ab0acd70e04a3878
Nhan Nguyen <nptn90@gmail.com>
2023-09-30 14:22:02 +0700

    Map

---
+++ src/test/demo/TestObject.java
@@ -0,0 +1,26 @@
+package test.demo;
+
+import java.util.Objects;
+
+public class TestObject {
+    private final String value;
+    private final Integer number;
+
+    public TestObject(String value, Integer number) {
+        this.value = value;
+        this.number = number;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TestObject that = (TestObject) o;
+        return Objects.equals(value, that.value) && Objects.equals(number, that.number);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value, number);
+    }
+}
---
+++ src/test/demo/collection/TestMap.java
@@ -0,0 +1,55 @@
+package test.demo.collection;
+
+import test.demo.TestObject;
+
+import java.util.*;
+
+public class TestMap {
+
+    public static void main(String[] args) {
+        /**
+         * HashMap: Key - Value
+         * Not duplicated key. Not ordered.
+         */
+        Map<String, Integer> testHashMap = new HashMap<>();
+        testHashMap.put("key 1", 1);
+        testHashMap.put("key 2", 1);
+        testHashMap.put("key 1", 2);
+        testHashMap.put("key 3", 3);
+        System.out.println("HashMap: " + testHashMap);
+
+        /**
+         * LinkedHashMap: Key - value
+         * Not duplicated. Ordered
+         */
+        Map<String, Integer> testLinkedHashMap = new LinkedHashMap<>();
+        testLinkedHashMap.put("key 1", 1);
+        testLinkedHashMap.put("key 2", 1);
+        testLinkedHashMap.put("key 1", 2);
+        testLinkedHashMap.put("key 3", 3);
+        System.out.println("LinkedHashMap: " + testLinkedHashMap);
+
+        /**
+         * TreeMap: Key - value
+         * Not duplicated. Ordered, can change the order by custom Comparator.
+         */
+        Map<String, Integer> testSortedMap = new TreeMap<>(Comparator.reverseOrder());
+        testSortedMap.put("key 1", 1);
+        testSortedMap.put("key 2", 1);
+        testSortedMap.put("key 1", 2);
+        testSortedMap.put("key 3", 3);
+        System.out.println("SortedMap: " + testSortedMap);
+
+        /**
+         * Test Custom key
+         * Override Equals Hashcode
+         */
+        Map<TestObject, Integer> testCustomKey = new HashMap<>();
+        testCustomKey.put(new TestObject("key 1", 1), 1);
+        testCustomKey.put(new TestObject("key 2", 1), 1);
+        testCustomKey.put(new TestObject("key 1", 1), 2);
+        testCustomKey.put(new TestObject("key 3", 3), 3);
+        System.out.println("TestCustomKey: " + testCustomKey.values());
+    }
+
+}
